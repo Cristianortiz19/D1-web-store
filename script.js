7d0e4a0ead61744ab05411958403a3a180d64eca
Valeria Martinez <1105360405@u.icesi.edu.co>
2023-03-07 17:43:32 -0500

    new changes to featured product

--- script.js
+++ script.js
@@ -2,3 +2,39 @@ document.getElementById("myButton").addEventListener("click", function() {
     alert("ola que tal");
   });
   
+  const buttons = document.querySelectorAll('.product-button');
+  const cartItems = document.querySelector('.cart-items');
+  
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const item = button.parentElement;
+      const itemTitle = item.querySelector('.product-title').innerText;
+      const itemPrice = item.querySelector('.product-price').innerText;
+      
+      const cartItem = document.createElement('div');
+      cartItem.classList.add('cart-item');
+      cartItem.innerHTML = `
+        <p>${itemTitle}</p>
+        <p>${itemPrice}</p>
+      `;
+      
+      cartItems.appendChild(cartItem);
+    });
+  });
+
+  const button = document.querySelector('.featured-button');
+
+
+button.addEventListener('click', () => {
+  const itemTitle = document.querySelector('.featured-title').innerText;
+  const itemPrice = document.querySelector('.featured-price').innerText;
+
+  const cartItem = document.createElement('div');
+  cartItem.classList.add('cart-item');
+  cartItem.innerHTML = `
+    <p>${itemTitle}</p>
+    <p>${itemPrice}</p>
+  `;
+
+  cartItems.appendChild(cartItem);
+});
