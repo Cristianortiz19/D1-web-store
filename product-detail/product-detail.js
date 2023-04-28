66019bf01c3fa65baac4a255171a253a3c4fc23a
Cristianortiz19 <cristclar012@gmail.com>
2023-04-28 01:06:48 -0500

    Product-detail

--- product-detail/product-detail.js
+++ product-detail/product-detail.js
@@ -1,4 +1,5 @@
 
+const cardProducts = document.querySelector('#product-cards');
 
 const url = window.location.search;
 const searchParas = new URLSearchParams(url);
@@ -16,5 +17,18 @@ async function getData(){
 }
 
 function organiceData(array){
-    
+    array.forEach(element => {
+        let comparision = element.name.replaceAll(" ", "-");
+        if(comparision == solicitude) {
+            const productObj = document.createElement('detail-element');
+            productObj.setAttribute('name', element.name);
+            productObj.setAttribute('url', element.url);
+            productObj.setAttribute('description', element.description);
+            productObj.setAttribute('price', element.price);
+            productObj.setAttribute('category', element.category);
+            productObj.setAttribute('brand', element.brand);
+            cardProducts.append(productObj);
+        }
+    });
 }
+getData();
